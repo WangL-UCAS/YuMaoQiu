8cf824fd71ee6e37b4f0b6009f625238b094fb30
WangL <109268466+WangL-UCAS@users.noreply.github.com>
2026-06-06 05:10:47 +0800

    Delete pythonProject directory

--- pythonProject/jiema.py
+++
@@ -1,5 +0,0 @@
-import base64
-
-value = "hgHXqwQHD8+BfGWe/3LHr34kQ5ekCaDCmcK07yu8B6+oYoXqb0j3k6MVbxt/urWGd4NGEawnkNNlgIs+lez4Y8gjw0kp3pBsyouVZMiSGzU="
-decoded = base64.b64decode(value)
-print(decoded)
